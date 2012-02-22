e634eec38ddf6e66feb1bbe2517d3375be82fa7f
saifei <saifei.xiao@sen5.com>
2012-02-22 14:56:08 +0800

    add enter andriod system function in menu.  main menu-->standby/restart->>Enter Andriod system.

---
+++ lib/python/Screens/changesystem.py
@@ -0,0 +1,8 @@
+from Screens.MessageBox import MessageBox
+
+class changesystem(MessageBox):
+	def __init__(self, session):
+		MessageBox.__init__(self, session, _("When you do a change system, the system will reboot"
+			"and change into Andriod system\n"
+			"Really do a change system?"), MessageBox.TYPE_YESNO)
+		self.skinName = "MessageBox"
