6a0262fba153feb5775012bf519e58464390d90d
Archer Zhao <archer.zhao@sen5.com>
2012-06-26 20:52:33 +0800

    Update Screens/About.py and Components/About.py from openpli official git repository.
    Version:
    3ad05a368f6f05ef93d713c2c5f0f79fd9f50090

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,48 +1,51 @@
-from Tools.Directories import resolveFilename, SCOPE_SYSETC
-from enigma import getEnigmaVersionString
-from os import popen
+import sys
+import os
+import time
 
-class About:
-	def __init__(self):
+def getVersionString():
+	return getImageVersionString()
+
+def getImageVersionString():
+	try:
+		st = os.stat('/usr/lib/ipkg/status')
+		tm = time.localtime(st.st_mtime)
+		if tm.tm_year >= 2011:
+			return time.strftime("%b %e %Y %H:%M:%S", tm)
+	except:
 		pass
+	return _("unavailable")
 
-	def getVersionString(self):
-		return self.getImageVersionString()
+def getEnigmaVersionString():
+	import enigma
+	enigma_version = enigma.getEnigmaVersionString()
+	if '-(no branch)' in enigma_version:
+		enigma_version = enigma_version [:-12]
+	return enigma_version
 
-	def getImageVersionString(self):
-		try:
-			file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
-			lines = file.readlines()
-			for x in lines:
-				splitted = x.split('=')
-				if splitted[0] == "version":
-					#     YYYY MM DD hh mm
-					#0120 2005 11 29 01 16
-					#0123 4567 89 01 23 45
-					version = splitted[1]
-					year = version[-13:-9]
-					month = version[-9:-7]
-					day = version[-7:-5]
-					name = version[1:-13]
+def getKernelVersionString():
+	try:
+		return open("/proc/version","r").read().split(' ', 4)[2].split('-',2)[0]
+	except:
+		return _("unknown")
 
-					return '-'.join(([name, year, month, day]))
-			file.close()
-		except IOError:
-			pass
+def getHardwareTypeString():
+	try:
+		if os.path.isfile("/proc/stb/info/boxtype"):
+			return open("/proc/stb/info/boxtype").read().strip().upper() + " (" + open("/proc/stb/info/board_revision").read().strip() + "-" + open("/proc/stb/info/version").read().strip() + ")"
+		if os.path.isfile("/proc/stb/info/vumodel"):
+			return "VU+" + open("/proc/stb/info/vumodel").read().strip().upper() + "(" + open("/proc/stb/info/version").read().strip().upper() + ")" 
+		if os.path.isfile("/proc/stb/info/model"):
+			return open("/proc/stb/info/model").read().strip().upper()
+	except:
+		pass
+	return _("unavailable")
 
-		return "unavailable"
+def getImageTypeString():
+	try:
+		return open("/etc/issue").readlines()[-2].capitalize().strip()[:-6]
+	except:
+		pass
+	return _("undefined")
 
-	def getEnigmaVersionString(self):
-		return getEnigmaVersionString()
-
-	def getKernelVersionString(self):
-		try:
-			result = popen("uname -r","r").read().strip("\n").split('-')
-			kernel_version = result[0]
-			return kernel_version
-		except:
-			pass
-
-		return "unknown"
-
-about = About()
+# For modules that do "from About import about"
+about = sys.modules[__name__]
