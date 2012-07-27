14a0b6d03e77b7f160f97e74ccfca73b3f9abd5e
saifei <saifei.xiao@sen5.com>
2012-07-27 19:11:17 +0800

    Fix network test cause system crashe.

--- lib/python/Plugins/Extensions/FactoryTest/NetworkTest.py
+++ lib/python/Plugins/Extensions/FactoryTest/NetworkTest.py
@@ -6,6 +6,8 @@ from Components.Sources.StaticText import StaticText
 from Components.ActionMap import ActionMap, NumberActionMap
 from FactoryTestPublic import *
 import time
+from enigma import eTimer
+
 class NetworkTest(Screen):
 	
 	skin = """
@@ -20,13 +22,13 @@ class NetworkTest(Screen):
 			<eLabel text="Network Test" position="270,20" size="540,43" font="Regular;35" halign="right" foregroundColor="black" backgroundColor="grey" transparent="1" />
 			<widget source="workstatus" render="Label" position="110,145" size="700,70" font="Regular;26" foregroundColor="yellow" backgroundColor="transpBlack" transparent="1" />
 			<widget source="testinfo" render="Label" position="120,230" size="660,330" font="Regular;15" backgroundColor="transpBlack" transparent="1" />
-			<ePixmap pixmap="DMConcinnity-HD-Transp/buttons/green.png" position="30,570" size="35,27" alphatest="blend" />
-			<eLabel text="Start" position="80,573" size="200,26" zPosition="1" font="Regular;22" halign="left" foregroundColor="black" backgroundColor="grey" transparent="1" />
 		</screen>"""
 #			<widget source="usbtitle" render="Label" position="110,145" size="700,35" font="Regular;26" foregroundColor="yellow" backgroundColor="transpBlack" transparent="1" />
 #			<widget source="usbname" render="Label" position="140,190" size="700,30" font="Regular;24" backgroundColor="transpBlack" transparent="1" />
 #			<widget source="usbsize" render="Label" position="140,220" size="700,30" font="Regular;24" backgroundColor="transpBlack" transparent="1" />
-#			<widget source="usbfreesize" render="Label" position="140,250" size="700,30" font="Regular;24" backgroundColor="transpBlack" transparent="1" />		
+#			<widget source="usbfreesize" render="Label" position="140,250" size="700,30" font="Regular;24" backgroundColor="transpBlack" transparent="1" />	
+#			<ePixmap pixmap="DMConcinnity-HD-Transp/buttons/green.png" position="30,570" size="35,27" alphatest="blend" />
+#			<eLabel text="Start" position="80,573" size="200,26" zPosition="1" font="Regular;22" halign="left" foregroundColor="black" backgroundColor="grey" transparent="1" />
 	def __init__(self,session,testitem):
 		Screen.__init__(self, session)
 		self.testserver = "8.8.8.8"
@@ -34,22 +36,25 @@ class NetworkTest(Screen):
 		self._runing = False
 		self.result = False
 		
-		self["workstatus"] = StaticText("Check the network cable is connected,Press green\n key Start Test")
+		self["workstatus"] = StaticText("Check the network cable is connected,Press OK\n key Start Test")
 		self["testinfo"] = StaticText("")
 		self["teststatus"] = StaticText("Start")
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"], 
 			{
-				"green": self.startTest,
-				"ok": self.doclose
+				"cancel": self.doclose,
+				"ok": self.startTest
 			})
 			
+		self.testTimer = eTimer()
+		self.testTimer.callback.append(self.pingServer)
+		
 	def startTest(self):
 		print "key green"
-		if self._runing:
-			return 
-		else:
-		   self.pingServer()
-		   
+		if self._runing == False:
+			self["workstatus"].setText("Network Connecting ....")
+			self["testinfo"].setText("")
+			self.testTimer.start(1000)
+		   	
 	def doclose(self):
 		self.updeteResult()
 		self.close()
@@ -60,12 +65,6 @@ class NetworkTest(Screen):
 		else:
 			self.testitem.setTestResult(FactoryTestItem.TESTRESULT_ERROR)
 			
-	def test(self):
-		self._runing = True
-		print "test"
-		time.sleep(5)
-		self._runing = False
-		
 	def dhcp(self):
 		cmd = "ifconfig eth0 down"
 		p = os.popen(cmd)
@@ -73,26 +72,70 @@ class NetworkTest(Screen):
 		print "info read",info
 
 	def pingServer(self):
-		self["workstatus"].setText("Ping Server .........")
-		
-		cmd = "ping -c 3 %s" % self.testserver
-		p = os.popen(cmd)
-		info = p.read()
-		print "info read",info
-		p.close()
-
-		self["testinfo"].setText(info)
-
-		re_lost_str = '(\d+)% packet loss'
-		lost_packet = int(re.search(re_lost_str,info).group(1))
-		print "lost package is :",lost_packet
-		if lost_packet == 100:
-			self["workstatus"].setText("Network Connection Error!!! Check cable and Hardware")
-			print "device Error"
+		self.testTimer.stop()
+		self._runing = True
+		lost_packet = 100
+		try:
+			cmd = "ping -c 3 %s" % self.testserver
+			p = os.popen(cmd)
+			info = p.read()
+			print "info read",info
+			p.close()
+			self["testinfo"].setText(info)
+		except:
+			print "exception"
+			print "Network Connection Error!!! Check cable and Hardware"
 			self.result = False
 #			return False
-		self["workstatus"].setText("Network Connection OK")
-		self.result = True
+
+		if info == "":
+			self["workstatus"].setText("Network Connection Error!!! Check cable and Hardware")
+			print "Network Connection Error!!! Check cable and Hardware"
+			self.result = False
+#			return False
+		else:
+			try:
+				re_lost_str = '(\d+)% packet loss'
+				lost_packet = int(re.search(re_lost_str,info).group(1))
+				print "lost package is :",lost_packet
+			except:
+				self["workstatus"].setText("Network Connection Error!!! Check cable and Hardware")
+				print "Network Connection Error!!! Check cable and Hardware"
+				self.result = False
+#				return False
+			if lost_packet == 100:
+				self["workstatus"].setText("Network Connection Error!!! Check cable and Hardware")
+				print "Network Connection Error!!! Check cable and Hardware"
+				self.result = False
+#				return False
+			else:
+				self["workstatus"].setText("Network Connection OK")
+				print "Network Connection OK"
+				self.result = True
+#				return True
+		self._runing = False
+		return self.result
+#		self["workstatus"].setText("Ping Server .........")
+#		
+#		cmd = "ping -c 3 %s" % self.testserver
+#		p = os.popen(cmd)
+#		info = p.read()
+#		print "info read",info
+#		p.close()
+#
+#		self["testinfo"].setText(info)
+#
+#		re_lost_str = '(\d+)% packet loss'
+#		lost_packet = int(re.search(re_lost_str,info).group(1))
+#		print "lost package is :",lost_packet
+#		if lost_packet == 100:
+#			self["workstatus"].setText("Network Connection Error!!! Check cable and Hardware")
+#			print "device Error"
+#			self.result = False
+#			return False
+#		else:
+#			self["workstatus"].setText("Network Connection OK")
+#			self.result = True
 		
 #		return True
 		
