eee9e8c8673e3d686d253253218eb593c8a92dab
saifei <saifei.xiao@sen5.com>
2012-09-29 18:23:42 +0800

    Add defualt service test in factory test menu(LNB test step 3 and 4).

--- lib/python/Plugins/Extensions/FactoryTest/LnbTest.py
+++ lib/python/Plugins/Extensions/FactoryTest/LnbTest.py
@@ -6,11 +6,10 @@ from Components.ActionMap import ActionMap, NumberActionMap
 from Components.Sources.StaticText import StaticText
 from Components.MultiContent import MultiContentEntryText, MultiContentEntryPixmapAlphaTest
 from Tools.LoadPixmap import LoadPixmap
-from enigma import eListboxPythonMultiContent, gFont
-
+from enigma import eListboxPythonMultiContent, gFont,eTimer
 import array, fcntl, struct, termios, os
 import time
-               
+from Components.Console import Console
 class LnbTest:
 
 	FE_SET_VOLTAGE = 28483
@@ -21,17 +20,22 @@ class LnbTest:
 	diseqcstep = 0
 
 	def __init__(self,lnbindex):
+		self.lnbindex = lnbindex
 		self.lnvdev = "/dev/dvb/adapter0/frontend%d" %lnbindex
-		self.voltageTestlist = [self.setVoltage13,self.setVoltage18]
+		self.voltageTestlist = [self.setVoltage13,self.setVoltage18,self.playSevice,self.playSevice]
 		self.dvbtvoltageTestlist = [self.setVoltage5dvbt,self.setVoltage0dvbt]
 		
 		self.diseqcTestList = [self.setDiSEqC]*4
 		self.tunertype = "DVB-S"
 		if lnbindex == 2:
 			self.tunertype = "DVB-T"
-			
+		self.playsevice = False
+		
+		self.playTimer = eTimer()
+		self.playTimer.callback.append(self.doPlay)
+		self.playConsole = Console()
 	def checkVoltageTestFinish(self):
-		if LnbTest.voltageStep in [0,1]:
+		if LnbTest.voltageStep in [0,1,2,3]:
 			return False
 		else:
 			return True
@@ -39,11 +43,9 @@ class LnbTest:
 	def testVoltage(self,first):
 		if first:
 			LnbTest.voltageStep = 0
-		if LnbTest.voltageStep not in [0,1]:
+		if LnbTest.voltageStep not in [0,1,2,3]:
 			LnbTest.voltageStep = 0
-		
-		LnbTest.voltageStep += 1
-		step = LnbTest.voltageStep - 1
+		step = LnbTest.voltageStep
 		if self.tunertype == "DVB-S":
 			return self.voltageTestlist[step]()
 		elif self.tunertype == "DVB-T":
@@ -60,22 +62,24 @@ class LnbTest:
 		except IOError:
 			print "Fail,Check hardware!!"
 		if test == 0:
-			testresult = "set:Voltage 13V,"
+			testresult = "Set voltage 13V ok\n"
 		else:
-			return "set Voltage 13V fail,Check hardware!!"
+			LnbTest.voltageStep +=1
+			return "Fail,Check hardware!!"
 #22k on
 		try:
 			test  = fcntl.ioctl(lnbfd.fileno(),LnbTest.FE_SET_TONE,0)	
 			lnbfd.close()
 		except IOError:
 			print "Fail,Check hardware!!"
-			
 		if test == 0:
-			testresult += "22K on"
-			return testresult
+			testresult += "Set 22K on ok"
 		else:
-			return "set 22K on fail,Check hardware!!"
+			testresult = "Fail,Check hardware!!"
 			
+		LnbTest.voltageStep +=1
+		return testresult
+		
 	def setVoltage18(self):
 		print "set Voltage 18V"
 		test = -1
@@ -85,22 +89,86 @@ class LnbTest:
 		except IOError:
 			print "Fail,Check hardware!!"
 		if test == 0:
-			testresult = "set:Voltage 18V,"
+			testresult = "Set voltage 18V ok\n"
 		else:
-			return "set Voltage 18V fail,Check hardware!!"
+			LnbTest.voltageStep +=1
+			return "Fail,Check hardware!!"
 #set 22k on
 		try:
 			test  = fcntl.ioctl(lnbfd.fileno(),LnbTest.FE_SET_TONE,0)	
 			lnbfd.close()
 		except IOError:
-			print "Fail,Check hardware!!"
-			
+			print "Fail,Check hardware!!"	
 		if test == 0:
-			testresult += "22K on"
-			return testresult
+			testresult += "Set 22K on ok"
 		else:
-			return "set 22K off fail,Check hardware!!"
+			return "Fail,Check hardware!!"
+			
+		LnbTest.voltageStep +=1	
+		return testresult
+		
+	def playSevice(self):
+		if self.playsevice == False:
+			self.playsevice = True
+			if self.lnbindex == 0:
+				self.tunerid = "0"
+			else:
+				self.tunerid = "1"
+			if LnbTest.voltageStep == 2:
+				self.frq = "3660"
+				self.sr = "27500"
+				self.vpid = "2317"
+				self.apid = "2318"
+				self.pcrpid= "2317"
+				self.voltage = "0"
+				testresult = "13V on Play service:\n" +"Name: ALKASS"
+			else:
+				self.frq = "3960"
+				self.sr = "27500"
+				self.vpid = "69"
+				self.apid = "68"
+				self.pcrpid= "69"
+				self.voltage = "1"
+				testresult = "18V on Play service:\n" +"Name: VKT TEST"
+			self.playTimer.start(1)
+		else:
+			testresult = "Wait for last sevice play end!"
+		return testresult
 
+	def doPlay(self):
+		self.playTimer.stop()
+		print "play start"
+		
+		clearcmd = "dvb_test "+self.tunerid + " "+self.frq+" "+self.sr+" "+self.vpid+" "+self.apid+" "+self.pcrpid+ " 5 "+self.voltage
+		os.system(clearcmd)
+		
+		LnbTest.voltageStep +=1	
+		self.playsevice = False
+		print "play finish!"
+	#	cmds = []
+	#	cmds.append("0\n")
+	#	cmds.append("./dvb_test " + "0 1500 27500 600 601 600 5")
+	#	self.playConsole.eBatch(cmd, self.doplaycb)
+	#	if self.lnbindex == 0:
+	#		clearcmd = "./dvb_test 1 "+self.frq+" "+self.sr+" "+self.vpid+" "+self.apid+" "+self.pcrpid+ " 0"
+	#	else:
+	#		clearcmd = "./dvb_test 0 "+self.frq+" "+self.sr+" "+self.vpid+" "+self.apid+" "+self.pcrpid+ " 0"
+	#	self.playConsole.ePopen(clearcmd, self.doclerncb)
+	#	print "play start!"
+	#	playcmd = "./dvb_test "+self.tunerid+" "+self.frq+" "+self.sr+" "+self.vpid+" "+self.apid+" "+self.pcrpid+ " 5" 
+	#	self.playConsole.ePopen(playcmd, self.doplaycb)
+		
+	def doclerncb(self,result,retval=None, extra_args=None):
+		print "clear!"
+		print "play start!"
+		playcmd = "./dvb_test "+self.tunerid+" "+self.frq+" "+self.sr+" "+self.vpid+" "+self.apid+" "+self.pcrpid+ " 5" 
+		self.playConsole.ePopen(playcmd, self.doplaycb)
+		
+	def doplaycb(self,result,retval=None, extra_args=None):		
+		LnbTest.voltageStep +=1	
+		self.playsevice = False
+		print "play finish!"
+		
 	def setVoltage5dvbt(self):
 		print "set DVB-t Voltage 5V"
 		test = -1
