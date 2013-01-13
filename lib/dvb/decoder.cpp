1f5ddceed820a235d004b45b6243915686f73a12
saifei <saifei.xiao@sen5.com>
2013-01-13 11:15:05 +0800

    Recover code fix can't get Teletext data from Demux.

--- lib/dvb/decoder.cpp
+++ lib/dvb/decoder.cpp
@@ -1012,7 +1012,7 @@ int eTSMPEGDecoder::setState()
 			{
 				uint8_t demux = 0;
 				m_demux->getCADemuxID(demux);
-				demux = 3;
+				//demux = 3;
 				eTuxtxtApp::getInstance()->startCaching(m_textpid, demux);
 			}
 		}
